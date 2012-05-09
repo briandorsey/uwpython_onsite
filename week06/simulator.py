f5ede429cdc4acce783f23e2d74cfd73986819a5
Brian Dorsey <brian@dorseys.org>
2012-05-09 12:22:10 -0700

    simulator files

---
+++ week06/simulator.py
@@ -0,0 +1,48 @@
+"""
+Traffic simulation
+From Jon Jacky's Intro to Python course:
+    http://staff.washington.edu/jon/python-course/
+"""
+
+from vehicle import *
+from pedestrian import pedestrian
+from random import randint
+
+WIDTH = 65  # portion of one-dimensional space included in view
+
+VEHICLE_COUNT = 2
+SAFE_VEHICLE_COUNT = 0
+PEDESTRIAN_COUNT = 1
+
+vehicles = []
+for i in range(VEHICLE_COUNT):
+    vehicles.append(Vehicle(randint(1, WIDTH - 1),
+                            randint(-4, 4)))
+
+for i in range(SAFE_VEHICLE_COUNT):
+    vehicles.append(SafeVehicle(randint(1, WIDTH - 1),
+                                randint(-4, 4)))
+
+for v in vehicles:
+    print v, '   ',
+print
+
+
+def clear(view):
+    for i in range(WIDTH):
+        view[i] = '.'
+
+
+nsteps = 10
+stoplight = 'G'  # green, go
+view = list('.' * WIDTH)
+for t in range(nsteps):
+    clear(view)
+    if t == 6:
+        stoplight = 'R'  # red, stop
+        for i in range(PEDESTRIAN_COUNT):
+            vehicles.append(pedestrian(randint(1, WIDTH - 1)))
+    for v in vehicles:
+        v.move(signals=stoplight)
+        v.draw(view)
+    print ''.join(view), stoplight  # generate string from view
---
+++ week06/vehicle.py
@@ -0,0 +1,73 @@
+"""
+Vehicles for traffic simulation
+From Jon Jacky's Intro to Python course:
+    http://staff.washington.edu/jon/python-course/
+"""
+
+
+class Vehicle(object):
+    """
+    Vehicle base class for traffic simulation
+    """
+    census = 0  # number of Vehicles created
+
+    def __init__(self, position=0, velocity=0, icon='V'):
+        self.x = position  # Just one dimension for now
+        self.v = velocity  # negative v moves toward smaller x (left)
+        self.icon = '%s%s' % (icon, Vehicle.census)  # ID string
+        Vehicle.census += 1
+
+    def __str__(self):
+        return '%s: x %s, v %s' % (self.icon, self.x, self.v)
+
+    def move(self, dt=1, signals=None):
+        """
+        move self for time interval dt, update position
+        possibly respond to signals from the environment
+        """
+        self.x += dt * self.v
+
+    def draw(self, view):
+        """
+        draw self at position in view
+        """
+        l = len(self.icon)
+        if 0 < self.x < len(view) - l:
+            view[self.x:self.x + l] = self.icon
+
+
+class SafeVehicle(Vehicle):
+    """
+    Safe Vehicle subclass of Vehicle base class, change move behavior
+    """
+    def __init__(self, position=0, velocity=0, icon='S'):
+        Vehicle.__init__(self, position, velocity, icon)
+
+    def move(self, dt=1, signals=None):
+        """
+        Do not move when 'R' (red light) in signals
+        """
+        if 'R' not in signals:
+            self.x += dt * self.v
+
+
+if __name__ == '__main__':
+    v0 = Vehicle()
+    v1 = Vehicle(10)
+    v2 = Vehicle(30, -20)
+    v3 = SafeVehicle(20, 5)
+    print v0
+    print v1
+    print v2
+    print v3
+    v0.move()
+    v1.move()
+    stoplight = ('R',)  # red light
+    v2.move(signals=stoplight)  # ignore
+    v3.move(signals=stoplight)  # obey
+    print 'After move:'
+    print v0
+    print v1
+    print 'After move with red stoplight:'
+    print v2
+    print v3
