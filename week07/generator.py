aa8a658106c4f4f50ab591af868ca67e56642362
Brian Dorsey <brian@dorseys.org>
2012-05-16 11:45:10 -0700

    case studies from Jon

---
+++ week07/generator.py
@@ -0,0 +1,59 @@
+"""
+Demonstrate generators and lazy computation
+From Jon Jacky's Intro to Python course:
+    http://staff.washington.edu/jon/python-course/
+"""
+
+
+# review range
+print range(10)
+
+# Print a million elements, type ^C to stop
+# for i in range(1000000): print i
+
+# Try to print a billion elements, may fail with MemoryError
+# for i in range(1000000000): print i
+
+# Use xrange not range for a billion elements, type ^C to stop
+# for i in xrange(1000000000): print i
+
+# range is eager - computes all the elements first
+# xrange is lazy - computes each element as needed
+
+# Python generators code lazy computation,
+# remember values of local variables between calls
+
+
+def my_xrange(size):
+    i = -1
+    while True:
+        i += 1
+        if i < size:
+            yield i  # yield not return makes generator
+        else:
+            return   # StopIteration is automatically raised on return
+
+for i in my_xrange(3):
+    print i
+
+# generate a billion elements
+# for i in my_xrange(1000000000): print i  # ^C to stop
+
+# Generator expressions are like list comprehensions, but lazy
+
+# one hundred million elements
+# list comprehension may fail with MemoryError after 20+ sec
+# sum([2*i for i in xrange(100000000)])
+
+# one hundred million elements
+# generator expression returns result after 20+ sec
+# sum(2*i for i in xrange(100000000))
+
+import time
+
+def timer(count):
+    i = 0
+    while i < count:
+        i += 1
+        time.sleep(1)
+        yield i
